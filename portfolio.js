46ef61ed52b309df3aab6ad9a61184b757c9d567
KumarSaurabhThakur <160490638+KumarSaurabhThakur@users.noreply.github.com>
2025-04-29 14:29:06 +0530

    Update portfolio.js

---
+++ portfolio.js
@@ -0,0 +1,13 @@
+Promise.all(Array.from(document.images).map(img => {
+    if (img.complete)
+        return Promise.resolve(img.naturalHeight !== 0);
+    return new Promise(resolve => {
+        img.addEventListener('load', () => resolve(true));
+        img.addEventListener('error', () => resolve(false));
+    });
+})).then(results => {
+    if (results.every(res => res))
+        console.log('all images loaded successfully');
+    else
+        console.log('some images failed to load, all finished loading');
+});
